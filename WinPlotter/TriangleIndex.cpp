83aa82341bd6691a207d9ca300797853b8b6f99c
Kirill Yanykin <yanykin@ya.ru>
2014-10-07 12:44:17 +0400

    Должна быть исправлена кодировка исходников на UTF-8

--- WinPlotter/TriangleIndex.cpp
+++ WinPlotter/TriangleIndex.cpp
@@ -1,10 +1,10 @@
-#include "TriangleIndex.h"
+п»ї#include "TriangleIndex.h"
 
 
 CTriangleIndex::CTriangleIndex(int first, int second, int third) :
 First(first), Second(second), Third(third)
 {
-	// Проверяем корректность индекса
+	// РџСЂРѕРІРµСЂСЏРµРј РєРѕСЂСЂРµРєС‚РЅРѕСЃС‚СЊ РёРЅРґРµРєСЃР°
 	if (first < 0 || second < 0 || third < 0 || first == second || first == third || second == third)
 	{
 		throw CTriangleIndex::IncorrectIndex();
